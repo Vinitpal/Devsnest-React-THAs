817c27fcfe843932e91efa51b7525f4e575efdec
Vinit <vs361017@gmail.com>
2021-07-08 12:46:38 +0530

    build commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
+  // {/* </BrowserRouter>, */}
   document.getElementById("root")
 );
